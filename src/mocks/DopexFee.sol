794deb3c4bf9cd4e6624b84323293f3fd666f0b3
WitherBlock <witherblock@protonmail.com>
2023-11-08 01:22:54 +0400

    chore: remove mock unused contract

--- src/mocks/DopexFee.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-contract DopexFee {
-    function onFeeReqReceive() external view returns (uint256) {
-        return 0;
-    }
-}
