3ff63a1331bdbd56984c1f963e1ee952288afb95
WitherBlock <witherblock@protonmail.com>
2023-11-12 03:04:35 +0400

    chore: cleanup option market deploy script

--- script/DeployOptionMarket.s.sol
+++ script/DeployOptionMarket.s.sol
@@ -7,8 +7,7 @@ import {DopexV2OptionMarket} from "../src/DopexV2OptionMarket.sol";
 contract DeployOptionMarket is Script {
     function run() public {
         address optionPricing = 0x2b99e3D67dAD973c1B9747Da742B7E26c8Bdd67B;
-        address pm = 0xE4bA6740aF4c666325D49B3112E4758371386aDc; // Change
-        address dpFee = address(0);
+        address pm = 0xE4bA6740aF4c666325D49B3112E4758371386aDc;
         address callAsset = 0x912CE59144191C1204E64559FE8253a0e49E6548;
         address putAsset = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
         address primePool = 0xC6F780497A95e246EB9449f5e4770916DCd6396A;
@@ -45,7 +44,7 @@ contract DeployOptionMarket is Script {
         om.updateAddress(
             address(0),
             address(0),
-            dpFee,
+            address(0),
             optionPricing,
             msg.sender,
             true,
