ca97e4b2e29b20f136789a8717353ec8dafbe13c
WitherBlock <witherblock@protonmail.com>
2024-01-07 04:34:03 +0400

    feat: deploy option pricing linear

---
+++ script/DeployOptionPricingLinear.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+import {OptionPricingLinear} from "../src/pricing/OptionPricingLinear.sol";
+
+contract DeployOptionPricingLinear is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+        OptionPricingLinear opl = new OptionPricingLinear(1e4, 1e3);
+        console.log(address(opl));
+        vm.stopBroadcast();
+    }
+}
