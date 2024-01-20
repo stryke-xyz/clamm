287e3cf2c7bd9efc7ba3fbb0aacef54657735d1e
Aercwarden <arc@dopex.io>
2024-01-21 00:27:59 +0530

    fix: deploy script

--- script/DeployLimitExercise.s.sol
+++ script/DeployLimitExercise.s.sol
@@ -4,11 +4,20 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {LimitExercise} from "../src/periphery/LimitExercise.sol";
 
-contract DeployAutoExercise is Script {
+contract DeployLimitExercise is Script {
     function run() public {
         vm.startBroadcast();
-        LimitExercise aetb = new LimitExercise();
-        console.log(address(aetb));
+
+        address keeper = 0x662026937ae0dc84C0Ff32F8f7035d777A4f3CeB;
+
+        require(keeper != address(0), "keeper address zero in deploy script");
+
+        LimitExercise limitExercise = new LimitExercise();
+        
+        limitExercise.grantRole(limitExercise.KEEPER_ROLE(), keeper);
+        
+        console.log(address(limitExercise));
+
         vm.stopBroadcast();
     }
 }
