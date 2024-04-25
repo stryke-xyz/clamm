8c19ba2daf8fe9fffc03442489020469d996132c
psytama <psy@dopex.io>
2024-04-25 21:04:24 +0400

    fix: fixed clamm router

--- src/router/ClammRouter.sol
+++ src/router/ClammRouter.sol
@@ -7,20 +7,22 @@ import {IERC721Receiver} from "openzeppelin-contracts/contracts/token/ERC721/IER
 import {IOptionMarket} from "../interfaces/IOptionMarket.sol";
 
 import {Multicall} from "openzeppelin-contracts/contracts/utils/Multicall.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
- * @title DopexClammRouter
+ * @title ClammRouter
  * @author psytama
  * @dev Allow traders to buy CALL and PUT options using Dopex Option Market
  */
 contract ClammRouter is Multicall, IERC721Receiver {
+    using SafeERC20 for IERC20;
+
     // events
     event LogMintOption(
-        address user, address receiver, address optionMarket, uint256 tokenId, uint256 frontendId, uint256 referalId
+        address user, address receiver, address optionMarket, uint256 tokenId, uint256 frontendId, bytes32 referalId
     );
 
     // functions
-
     function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
         return this.onERC721Received.selector;
     }
@@ -30,13 +32,13 @@ contract ClammRouter is Multicall, IERC721Receiver {
         address optionMarket,
         address receiver,
         uint256 frontendId,
-        uint256 referalId
+        bytes32 referalId
     ) external {
         address token =
             _params.isCall ? IOptionMarket(optionMarket).callAsset() : IOptionMarket(optionMarket).putAsset();
 
         // transfer cost from user
-        IERC20(token).transferFrom(msg.sender, address(this), _params.maxCostAllowance);
+        IERC20(token).safeTransferFrom(msg.sender, address(this), _params.maxCostAllowance);
 
         // approve optionMarket to spend the cost
         IERC20(token).approve(optionMarket, _params.maxCostAllowance);
@@ -47,10 +49,10 @@ contract ClammRouter is Multicall, IERC721Receiver {
         // send the option to the receiver
         uint256 tokenId = IOptionMarket(optionMarket).optionIds();
 
-        IERC721(optionMarket).transferFrom(address(this), receiver, tokenId);
+        IERC721(optionMarket).safeTransferFrom(address(this), receiver, tokenId);
 
         // refund the remaining cost
-        IERC20(token).transfer(msg.sender, IERC20(token).balanceOf(address(this)));
+        IERC20(token).safeTransfer(msg.sender, IERC20(token).balanceOf(address(this)));
 
         // emit event
         emit LogMintOption(msg.sender, receiver, optionMarket, tokenId, frontendId, referalId);
