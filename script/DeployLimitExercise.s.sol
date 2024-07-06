eae6ca088aecec7434db2b6ce051503cb35d3b10
Aercwarden <arc@dopex.io>
2024-07-06 22:46:37 +0400

    refactor: deployment script

--- script/DeployLimitExercise.s.sol
+++ script/DeployLimitExercise.s.sol
@@ -4,18 +4,21 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {LimitExercise} from "../src/periphery/LimitExercise.sol";
 
+// source .env
+// forge script --chain arbitrum script/DeployLimitExercise.s.sol:DeployLimitExercise --rpc-url $ARBITRUM_RPC_URL --verify -vvvv --private-key $DEPLOYER_PRIVATE_KEY  --legacy --broadcast
 contract DeployLimitExercise is Script {
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-        address keeper = 0x662026937ae0dc84C0Ff32F8f7035d777A4f3CeB;
+        address keeper = address(0);
 
         require(keeper != address(0), "keeper address zero in deploy script");
 
         LimitExercise limitExercise = new LimitExercise();
-        
+
         limitExercise.grantRole(limitExercise.KEEPER_ROLE(), keeper);
-        
+
         console.log(address(limitExercise));
 
         vm.stopBroadcast();
