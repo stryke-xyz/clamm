9f9c144166c4bf0dd5f1fd49c7be7c60254ffdb6
Aercwarden <arc@dopex.io>
2024-07-15 20:05:27 +0400

    feat: router agnostic swapper

---
+++ script/DeployOnSwapReceiver.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {OnSwapReceiver} from "../src/swapper/OnSwapReceiver.sol";
+
+contract DeployOnSwapReceiver is Script {
+    function run() public {
+        vm.startBroadcast();
+        OnSwapReceiver swapper = new OnSwapReceiver();
+        swapper.setWhitelisted(address(0), true);
+        vm.stopBroadcast();
+    }
+}
